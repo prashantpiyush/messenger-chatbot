b24648a2f311f381be6128ca17ab1820274ae18e
Prashant Piyush <piyush.5862@gmail.com>
2017-12-23 02:52:33 +0530

    Syntax update

--- index.js
+++ index.js
@@ -1,30 +1,29 @@
-const express = require('express')
-const bodyParser = require('body-parser')
-const request = require('request')
+const express = require('express');
+const bodyParser = require('body-parser');
+const request = require('request');
 
 const app = express();
 
 const access = require('./access');
 
-const port = process.env.PORT || 8080;
+const PORT = process.env.PORT || 8080;
 
 app.use(express.static(__dirname));
-app.use(bodyParser.urlencoded({extended: false}))
-app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
 
-app.get('/', function(req, res) {
+app.get('/', (req, res) => {
   res.send('This is a bot');
 })
 
-app.get('/webhook/', function(req, res) {
+app.get('/webhook/', (req, res) => {
 	if (req.query['hub.verify_token'] === access.verify_token) {
-		res.send(req.query['hub.challenge'])
+		res.send(req.query['hub.challenge']);
 	}
-	res.send("Wrong token")
+	res.send("Wrong token");
 })
 
-app.post('/webhook/', function(req, res) {
-  console.log('\npost called new\n');
+app.post('/webhook/', (req, res) => {
 	let messaging_events = req.body.entry[0].messaging;
 	for (let i = 0; i < messaging_events.length; i++) {
 		let event = messaging_events[i]
@@ -34,11 +33,10 @@ app.post('/webhook/', function(req, res) {
 			sendText(sender, "Text echo: " + text.substring(0, 100))
 		}
 	}
-	res.sendStatus(200)
+	res.sendStatus(200);
 })
 
 function sendText(sender, text) {
-  console.log('\nsenText called\n');
 	let messageData = {text: text}
 	request({
 		url: "https://graph.facebook.com/v2.6/me/messages",
@@ -48,15 +46,15 @@ function sendText(sender, text) {
 			recipient: {id: sender},
 			message : messageData,
 		}
-	}, function(error, response, body) {
+	}, (error, response, body) => {
 		if (error) {
-			console.log("sending error")
+			console.log("sending error: " + error.message)
 		} else if (response.body.error) {
-			console.log("response body error")
+			console.log("response body error: " + response.body.error.message);
 		}
 	})
 }
 
-app.listen(port, function() {
-	console.log('app running on port: '  + port)
+app.listen(PORT, () => {
+	console.log(`App running on port: ${PORT}`);
 })
